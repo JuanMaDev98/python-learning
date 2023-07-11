2ab0b633a4411c45d59029f763407f8272ea5400
JuanMaDev <86654681+0MFCAT@users.noreply.github.com>
2023-07-11 16:39:46 -0400

    added the notes for data structures

---
+++ Python Developer Roadmap Files/Basic Sintaxis/unordered_sets.py
@@ -0,0 +1,9 @@
+# Un set es una estructura de datos desordenada lo que significa que cada vez que lo
+# llames podria devolver los valores en orden diferente
+
+conjunto = {1, 2, 3, 4, 5, 6}
+print(conjunto)
+conjunto.add(7)
+print(conjunto)
+conjunto.add(1)
+print(conjunto)
