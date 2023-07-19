87b254e79d54c5af168f2e1e3508e28ca3015800
JuanMaDev <86654681+0MFCAT@users.noreply.github.com>
2023-07-19 19:44:58 -0400

    added short_circuit tutorial

---
+++ Python Developer Roadmap Files/Basic Sintaxis/short_circuit.py
@@ -0,0 +1,7 @@
+# Short-circuit evaluation
+# Evaluación de corto circuito
+
+x = 1
+
+print(x == 1 and "OK" or "Not Ok")  # Retorna OK si se cumplen las dos primeras condiciones y Not OK si no se cumplen
+
